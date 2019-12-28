16307005675531c9ab62e6dcb6b567105c4cf14a
nodenota <nodenota@qq.com>
2019-12-28 11:34:00 +0800

    调整首页和页脚结构

--- website/pages/en/index.js
+++ website/pages/en/index.js
@@ -60,13 +60,10 @@ class HomeSplash extends React.Component {
 
     return (
       <SplashContainer>
-        <Logo img_src={`${baseUrl}img/undraw_monitor.svg`} />
         <div className="inner">
           <ProjectTitle siteConfig={siteConfig} />
           <PromoSection>
-            <Button href="#try">Try It Out</Button>
-            <Button href={docUrl('doc1.html')}>Example Link</Button>
-            <Button href={docUrl('doc2.html')}>Example Link 2</Button>
+            <Button href={docUrl('01开始/02文档规范.html')}>查看文档</Button>
           </PromoSection>
         </div>
       </SplashContainer>
@@ -197,12 +194,7 @@ class Index extends React.Component {
       <div>
         <HomeSplash siteConfig={siteConfig} language={language} />
         <div className="mainContainer">
-          <Features />
-          <FeatureCallout />
-          <LearnHow />
-          <TryOut />
-          <Description />
-          <Showcase />
+          
         </div>
       </div>
     );
